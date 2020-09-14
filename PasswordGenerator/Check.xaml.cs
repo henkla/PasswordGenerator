23242a63fd53804202849e5e54b0d28f8d6e06ba
henkla <lthlarsson@gmail.com>
2020-09-14 23:16:04 +0200

    init repo

---
+++ PasswordGenerator/Check.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for Check.xaml
+    /// </summary>
+    public partial class Check : Page
+    {
+        public Check()
+        {
+            InitializeComponent();
+        }
+    }
+}
